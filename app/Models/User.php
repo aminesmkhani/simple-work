1041bfb2bfdb48cb3a67792e742ea7e598fe2169
Amin <amin.esmkhani75@gmail.com>
2021-03-22 09:55:39 +0430

    simple relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,11 +43,11 @@ class User extends Authenticatable
 
     public function articles()
     {
-
+        return $this->hasMany(Article::class);
     }
 
     public function projects()
     {
-
+        return $this->hasMany(Project::class);
     }
 }
